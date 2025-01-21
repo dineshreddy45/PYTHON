193e25b89d32b592132263040b8cfdc769bf91ac
dineshreddy <dineshreddy.0053@gmail.com>
2025-01-21 11:06:19 +0530

    first one

---
+++ Data_types_in_Python.py
@@ -0,0 +1,13 @@
+#SWAPPING 2 number
+
+a = int(input("Enter the first number:- "))
+
+b = int(input("Enter the second number:- "))
+
+print ("Before swapping", a,b)
+
+c = b
+b = a
+a = c
+
+print ("after swapping values are ", a,b)
---
+++ basics_of_Python.py
@@ -0,0 +1,34 @@
+# playlist --> https://www.youtube.com/watch?v=DInMru2Eq6E&list=PLdo5W4Nhv31bZSiqiOL5ta39vSnBxpOPT&index=2
+
+
+#print function
+
+print ("hi welcome to python learning day1")
+
+#new line printing 
+
+print ("Hello World \n Hello World")
+
+#concatenation --> adding two strings using + operator
+
+print ("Hello" + " " + "Dinesh Kumar Reddy")
+
+
+#taking user input
+
+user_input = input ("enter the input you want to display:- ")
+
+print ("The output is in string format:-", user_input, type(user_input))
+
+
+#variables in python --> they are like containers to store some values [ just like a container which stores water]
+
+#Examples of variables 
+
+name = input ("enter your name:- ")
+
+print(name)
+
+#rules to name a variable 
+
+""" 1) hi """
